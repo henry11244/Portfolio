779372ebb14356c34472af260de51bf5e4633d87
Henry Nguyen <hln11244@gmail.com>
2022-08-24 17:28:44 -0700

    projects added to html template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div className="bucket-app">
+
+    </div>
+  );
+}
+
+export default App;
