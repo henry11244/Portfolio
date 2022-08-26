d297055e5819650e8e7ec71344c3a1ebd3693a66
Henry Nguyen <hln11244@gmail.com>
2022-08-25 21:21:58 -0700

    removed hrefs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 
 import React from 'react';
-import Projects from './components/projects';
-import Resume from './components/resume';
-import Contacts from './components/contact';
-import AboutMe from './components/aboutMe';
+// import Projects from './components/projects';
+// import Resume from './components/resume';
+// import Contacts from './components/contact';
+// import AboutMe from './components/aboutMe';
+import Main from './components/main';
 
-const currentPath = window.location.pathname
-const pathFunction = () => {
-  if (currentPath === '/Resume') { return (<Resume id='resume' />) }
-  else if (currentPath === '/Projects') { return <Projects id='project' /> }
-  else if (currentPath === '/Contact') { return <Contacts id='Contacts' /> }
-  else if (currentPath === '/AboutMe') { return <AboutMe id='aboutMe' /> }
-  else { return <AboutMe id='aboutMe' /> }
-}
+
+
 
 function App() {
   const currentPath = window.location.pathname
   console.log(currentPath)
+
+  // const pathFunction = () => {
+  //   if (currentPath === '/Resume') { return (<Resume id='resume' />) }
+  //   else if (currentPath === '/Projects') { return <Projects id='project' /> }
+  //   else if (currentPath === '/Contact') { return <Contacts id='Contacts' /> }
+  //   else if (currentPath === '/AboutMe') { return <AboutMe id='aboutMe' /> }
+  //   else { return <AboutMe id='aboutMe' /> }
+  // }
+
   return (
 
-    <div className="main" class="d-flex justify-content-center col-12">
-      `{pathFunction()}`
-    </div>
+    // <div className="main" class="d-flex justify-content-center col-12">
+    //   `{pathFunction()}`
+    // </div>
+    <Main />
   );
 }
 
---
+++ src/components/main.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Projects from './projects';
+import Resume from './resume';
+import Contacts from './contact';
+import AboutMe from './aboutMe';
+
+function Main() {
+    const [page, setPage] = useState('');
+    // setPage('aboutMe')
+    console.log(page)
+    const pathFunction = () => {
+        if (page === 'resume') {
+            return <Resume />;
+        }
+        if (page === 'projects') {
+            return <Projects />;
+        }
+        if (page === 'contacts') {
+            return <Contacts />;
+        }
+        if (page === 'aboutMe') {
+            return <AboutMe />;
+        }
+        return <AboutMe />
+    }
+
+
+    return (
+
+        < div className="main" class='' >
+            <nav class="navbar navbar-expand-lg navbar-light bg-light col-12 justify-content-center">
+
+                <div class="collapse navbar-collapse col-12 d-flex flex-wrap justify-content-center" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <button type="button" class="btn btn-primary" onClick={() => setPage('aboutMe')}>About Me</button>
+                        </li>
+                        <li class="nav-item active">
+                            <button type="button" class="btn btn-primary" onClick={() => setPage('projects')}>Projects</button>
+                        </li>
+                        <li class="nav-item active">
+                            <button type="button" class="btn btn-primary" onClick={() => setPage('resume')}>Resume</button>
+                        </li>
+                        <li class="nav-item active">
+                            <button type="button" class="btn btn-primary" onClick={() => setPage('contacts')}>Contacts</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            {pathFunction()}
+
+        </div >
+    )
+
+
+}
+
+export default Main
