48351d5b74e896c28d1ea4cd2e806197c00d1754
Henry Nguyen <hln11244@gmail.com>
2022-09-20 14:24:38 -0700

    deploy

--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route
-          path="/"
-          element={<App />}
-          exact={false}
-          strict={false}
-        />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <App />
   ,
   document.getElementById('root')
 );
