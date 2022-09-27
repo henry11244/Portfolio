bc43eacc307fd9c2f30907b0a11e6f54d2c276b0
Henry Nguyen <hln11244@gmail.com>
2022-09-27 11:03:04 -0700

    updated about me paragraphs

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -9,14 +9,15 @@ const Nav = () => {
     const [activeNav, setActiveNav] = useState('#')
 
     return (
-        <nav>
-            {/* <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a> */}
-            <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}>🏠</a>
-            <a href="#aboutMe" onClick={() => setActiveNav('#aboutMe')} className={activeNav === '#aboutMe' ? 'active' : ''}>About</a>
-            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>Portfolio</a>
-            <a href="#resume" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>Resume</a>
-            <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</a>
-        </nav>
+        <div>
+            <nav>
+                <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === '#header' ? 'active' : ''}>🏠</a>
+                <a href="#aboutMe" onClick={() => setActiveNav('#aboutMe')} className={activeNav === '#aboutMe' ? 'active' : ''}>About</a>
+                <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>Portfolio</a>
+                <a href="#resume" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}>Resume</a>
+                <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</a>
+            </nav>
+        </div>
     )
 }
 
